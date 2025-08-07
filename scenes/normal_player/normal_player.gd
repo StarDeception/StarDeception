a72b2e2a1b945474390bec5d5b87b0e06c16517a
David Durieux <david@durieux.family>
2025-08-08 00:29:24 +0200

    Add label for player and debug info UI

--- scenes/normal_player/normal_player.gd
+++ scenes/normal_player/normal_player.gd
@@ -2,6 +2,10 @@ extends CharacterBody3D
 
 @onready var camera = $Camera3D
 
+@onready var labelx: Label = $UserInterface/LabelXValue
+@onready var labely: Label = $UserInterface/LabelYValue
+@onready var labelz: Label = $UserInterface/LabelZValue
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
@@ -20,6 +24,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		rotate_y(-event.relative.x * 0.005)
 		camera.rotate_x(-event.relative.y * 0.005)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+
 
 func _physics_process(delta: float) -> void:
 	if not is_multiplayer_authority(): return
@@ -43,3 +48,7 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+	labelx.text = str("%0.2f" % global_position[0])
+	labely.text = str("%0.2f" % global_position[1])
+	labelz.text = str("%0.2f" % global_position[2])
