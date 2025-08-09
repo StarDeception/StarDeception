741885be4abf143004a8ef28f1df1129bf73c754
Neoz Sagan <51477498+Neoz-JourneyLab@users.noreply.github.com>
2025-08-09 15:04:21 +0200

    Revert "commentaires pour le global chat"
    This reverts commit 5a05a45e3ac323a9c44a5daef84dbeac0b418bea.

--- ui/global_chat/GlobalChat.cs
+++ ui/global_chat/GlobalChat.cs
@@ -11,41 +11,24 @@ public partial class GlobalChat : PanelContainer {
     [Export] private OptionButton channelSelector;
     private bool isVisible = false;
 
-    enum channel_E {
-        general = 0,
-        direct_message = 1,
-        group = 2,
-        alliance = 3,
-        region = 4,
-        unspecified = 5,
-    }
-
     public override void _Ready() {
         Visible = true;
 
-        //si dans l'éditeur, affichage texte aléatoire pour démo
-        if (Engine.IsEditorHint() || OS.HasFeature("editor")) {
-            string lorem = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
-            Random random = new Random();
-            string[] text_users = new string[] { "NeozSagan", "ddurieu", "irong", "The_Moye" };
-            for (int i = 0; i < 30; i++) {
-                string user = text_users[random.Next() % text_users.Length];
-                channel_E channel = (channel_E)Enum.GetValues(typeof(channel_E)).GetValue(random.Next() % Enum.GetNames(typeof(channel_E)).Length);
-                ReceiveMesssageFromServer(lorem.Substr((random.Next() % lorem.Length) / 2, (random.Next() % lorem.Length) / 2), user, channel);
-            }
+        string lorem = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+        Random random = new Random();
+        string[] text_users = new string[] { "NeozSagan", "ddurieu", "irong", "The_Moye" };
+        for (int i = 0; i < 30; i++) {
+            string user = text_users[random.Next() % text_users.Length];
+            channel_E channel = (channel_E)Enum.GetValues(typeof(channel_E)).GetValue(random.Next() % Enum.GetNames(typeof(channel_E)).Length);
+            ReceiveMesssageFromServer(lorem.Substr((random.Next() % lorem.Length) / 2, (random.Next() % lorem.Length) / 2), user, channel);
         }
 
-        //ajout des différents canaux pour l'onglet de sélection, et affiche par défaut le canal "général"
         foreach (string item in Enum.GetNames(typeof(channel_E))) {
             channelSelector.AddItem(item);
             channelSelector.Selected = 0;
         }
     }
 
-    /// <summary>
-    /// vérifie si on presse F12 pour ouvrir/fermer le chat
-    /// </summary>
-    /// <param name="delta"></param>
     public override void _Process(double delta) {
         if (Input.IsActionJustPressed("toggle_chat")) {
             isVisible = true;
@@ -58,30 +41,25 @@ public partial class GlobalChat : PanelContainer {
         }
     }
 
-    /// <summary>
-    /// lorsque l'on appuie sur entrée pour valider notre message
-    /// </summary>
-    /// <param name="nt">le texte à envoyer au serveur</param>
     private void _on_input_text_text_submitted(string nt) {
         if (string.IsNullOrWhiteSpace(nt)) return;
         SendMessageToServer(nt);
         inputField.Text = "";
     }
 
-    /// <summary>
-    /// Envoie un message (ici court circuité en local) avec en paramètre le canal sélectionné
-    /// </summary>
-    /// <param name="txt"></param>
     void SendMessageToServer(string txt) {
         ReceiveMesssageFromServer(txt, "NeozSagan", Enum.Parse<channel_E>(channelSelector.GetItemText(channelSelector.GetSelectedId())));
     }
 
-    /// <summary>
-    /// recoit un message au serveur précisant qui, quel canal, et quel message
-    /// </summary>
-    /// <param name="message"></param>
-    /// <param name="user_nick"></param>
-    /// <param name="channel"></param>
+    enum channel_E {
+        general = 0,
+        direct_message = 1,
+        group = 2,
+        alliance = 3,
+        region = 4,
+        unspecified = 5,
+    }
+
     private void ReceiveMesssageFromServer(string message, string user_nick, channel_E channel) {
         string gdh = DateTime.Now.Hour.ToString("00") + ":" + DateTime.Now.Minute.ToString("00") + ":" + DateTime.Now.Second.ToString("00");
 
@@ -89,18 +67,9 @@ public partial class GlobalChat : PanelContainer {
             $"{(channel == channel_E.unspecified ? "" : ("(" + channel.ToString() + ") "))}{message}[/color]\n");
     }
 
-    /// <summary>
-    /// couleurs forcées en exa selon les valeurs d'entrée pour éviter un hash aléatoire dans certains cas
-    /// </summary>
     Dictionary<string, string> forced_colors = new Dictionary<string, string>() {
         { channel_E.general.ToString(),  "FFFFFF" }
     };
-
-    /// <summary>
-    /// prends un string en entrée et envoie un code hexa de couleur aléatoire mais constant
-    /// </summary>
-    /// <param name="text"></param>
-    /// <returns></returns>
     private string GetHexaColorFromHash(string text) {
         if (forced_colors.ContainsKey(text)) return forced_colors[text];
 
