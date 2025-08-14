93387388e693be541f0eb2e20f8ee9c15b839590
GuillaumeCa <guicarre1@gmail.com>
2025-08-14 19:19:53 +0200

    Add dynamic collision generation and surface textures

--- scenes/planet/planet.gd
+++ scenes/planet/planet.gd
@@ -11,7 +11,7 @@ class_name Planet
 	{ "distance": 1000, "resolution": 10 },
 	{ "distance": 500, "resolution": 20 },
 ]
-@export var min_height: float
+@export var min_height: float = 10000.0
 @export var max_height: float
 @export var noise: FastNoiseLite
 @export var noise_micro: FastNoiseLite
@@ -27,7 +27,7 @@ class NoiseGenerator extends Resource:
 signal regenerate()
 
 func _on_update():
-	print("hello")
+	print("update")
 	regenerate.emit()
 
 func norm(value: float):
@@ -35,15 +35,18 @@ func norm(value: float):
 
 func get_height(point) -> Vector3:
 	var elev = 0.0
-	elev += norm(noise.get_noise_3dv(point * 400.0)) * 1.2 #* (1.0 + norm(noise_micro.get_noise_3dv(point * 100.0)) * 0.05)
+	elev += norm(noise.get_noise_3dv(point * 400.0)) * 0.3 #* (1.0 + norm(noise_micro.get_noise_3dv(point * 100.0)) * 0.05)
 	
 	
 	#for noise_map in noise_maps:
 		#elev += norm(noise_map.noise.get_noise_3dv(point * 100.0)) * noise_map.amplitude
+	elev = max(0.3, elev)
+	elev = min(0.35, elev)
 	
-	elev = max(1.0, elev)
-	elev -= clamp(noise.get_noise_3dv(point * 100.0) * 0.3, 0.3, 1.0)
+	elev += clamp(norm(noise.get_noise_3dv(point * 400.0)) * 0.27, 0.35, 0.5) #* (1.0 + norm(noise_micro.get_noise_3dv(point * 100.0)) * 0.05)
 	
-	elev *= 0.3
+	elev += norm(noise_micro.get_noise_3dv(point * 500)) * 0.01 #* (1.0 + norm(noise_micro.get_noise_3dv(point * 100.0)) * 0.05)
+	
+	#elev -= clamp(noise.get_noise_3dv(point * 100.0) * 0.3, 0.3, 1.0)
 	
 	return point * radius * (elev + 1.0)
